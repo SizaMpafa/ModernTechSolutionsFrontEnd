1d012d7a8e138f63b7aa24cac7a8df26453143fc
Siza-Mpafa030628 <sizampafa972@gmail.com>
2025-12-06 21:27:54 +0200

    updated RouteView for Employees

--- MTS_APP/src/components/EmployeesComponent.vue
+++ MTS_APP/src/components/EmployeesComponent.vue
@@ -10,12 +10,54 @@
 </script>
 
 <template>
-    <ul>
+    <!-- <ul>
         <li v-for="employee in workers" :key="employee.employeeId">{{ employee.name }}</li>
-    </ul>
+    </ul> -->
+
+    <div class="cards">
+        <div class="employee mb-2 border-top pt-3" v-for="employee in workers" :key="employee.employeeId">
+            <div class="d-flex">
+                <i class="fa-solid fa-user-tie mr-2"></i> 
+                <div class="nameNposition mr-6">
+                    <h6 class="pb-0">
+                        {{ employee.name }}
+                    </h6>
+
+                    <p class="text-lowercase  text-center font-italic">{{ employee.position }}</p>
+                </div>
+                <div class="contSal">
+                    <h5 class="contact">{{ employee.contact }}</h5>
+                    <p class="text-center font-italic">R{{ employee.salary }}</p>
+                </div>
+                <p class="btn btn-primary right-0">{{ employee.department }}</p>
+            </div>
+        </div>
+    </div>
 
 </template>
 
 <style>
+    .employee{
+        position: relative;
+    }
+    .right-0{
+        position: absolute;
+        right: 0;
+    }
+    .font-italic{
+        font-style: italic;
+        font-size: x-small;
+        margin-top: 0;
+        padding-top: 0;
+    }
+
+    .contact{
+        margin-left: 150px;
+        font-size: medium;
+    }
+
+    .fa-user-tie{
+        font-size: xx-large;
+    }
 
 </style>
